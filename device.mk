f71b54007938d4fe188ac2192dd020d22792ce00
shedowe19 <76975113+shedowe19@users.noreply.github.com>
2023-06-15 20:59:22 +0200

    Add files via upload

--- device.mk
+++ device.mk
@@ -1,10 +1,8 @@
-# Android device tree for samsung SM-A546B (a54x)
-
-```
 #
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := device/samsung/a54x
